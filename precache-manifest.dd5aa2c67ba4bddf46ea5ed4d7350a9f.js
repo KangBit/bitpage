c763ba759b9fedcff90c3baa89f88d417d7a748e
KangBit <kangbit@inbm.co.kr>
2020-04-18 16:50:26 +0900

    Updates

---
+++ precache-manifest.dd5aa2c67ba4bddf46ea5ed4d7350a9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163ac96dcfe3d4100f7657640e124460",
+    "url": "/bitpage/index.html"
+  },
+  {
+    "revision": "ae0dadb9fc46959b10c4",
+    "url": "/bitpage/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "15743975e4b67c41639c",
+    "url": "/bitpage/static/js/2.f48746c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bitpage/static/js/2.f48746c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae0dadb9fc46959b10c4",
+    "url": "/bitpage/static/js/main.a9bb479d.chunk.js"
+  },
+  {
+    "revision": "eaec1b3e27c0f8bd7abb",
+    "url": "/bitpage/static/js/runtime-main.69fb82f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bitpage/static/media/logo.5d5d9eef.svg"
+  }
+]);
